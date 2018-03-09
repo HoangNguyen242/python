0fc4630288287fc0043501eccba4148dc74853a9
Nguyen Ba Thanh <thanh@gmail.com>
2018-03-09 11:39:13 +0700

    update

--- exercises/List/main.py
+++ exercises/List/main.py
@@ -4,38 +4,38 @@
 
 # Main
 if __name__ == "__main__":
-    print "Main component"
+    print("Main component") 
 
 ## Khai bao
-list1 = ['physics', 'chemistry', 1997, 2000];
-list2 = [1, 2, 3, 4, 5 ];
-list3 = ["a", "b", "c", "d"];
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5 ]
+list3 = ["a", "b", "c", "d"]
 
 ## Truy cap List
-print "list1[0]: ", list1[0];
-print "list2[1:5]: ", list2[1:5];
+print("list1[0]: ", list1[0])
+print("list2[1:5]: ", list2[1:5])
 
 ## Cap nhat gia tri
-print "Gia tri tai vi tri 2 chuoi list2 : "
-print list2[2];
+print("Gia tri tai vi tri 2 chuoi list2 : ")
+print(list2[2])
 
-print "Cap nhat gia tri list2 : "
-list2[2] = 2001;
-print list2[2];
+print("Cap nhat gia tri list2 : ")
+list2[2] = 2001
+print(list2[2])
 
 ## Xoa phan list
-print "Xoa phan tu list1 : "
-print list1;
+print("Xoa phan tu list1 : ")
+print(list1)
 del list1[2]
-print list1;
+print(list1)
 
 ## Lay do dai list
-print "Do dai list1 : ",len(list1)
+print("Do dai list1 : ",len(list1))
 
 ## Them phan tu vao list
-print "Them phan tu list1 : "
-print list1
+print("Them phan tu list1 : ")
+print(list1)
 list1.append("Capnhat1")
-print list1
+print(list1)
 
 ##
--- exercises/List/thactac-list.py
+++ exercises/List/thactac-list.py
@@ -4,20 +4,20 @@
 
 # Main
 if __name__ == "__main__":
-    print "Main component"
+    print("Main component")
 
 list_used=['Thanh','Thang Long',15,'True']
-print "Chuoi tuong tac: ",list_used
+print("Chuoi tuong tac: ",list_used)
 
 ## Them phan tu
 list_used.append('Long Bien HN')
-print "Them phan tu: ",list_used
+print("Them phan tu: ",list_used)
 
 ## Tong phan tu
-print "Tong phan tu: ",list_used.count(list_used)
+print("Tong phan tu: ",list_used.count(list_used))
 
 ## Thay doi vi tri
 list_used.reverse()
-print "Thay doi vi tri: ",list_used
+print("Thay doi vi tri: ",list_used)
 
 ##
