509de9e1239cebdbd7b695e7d0655fe3985e6f53
Nguyen Ba Thanh <thanh@gmail.com>
2018-03-09 09:42:23 +0700

    update

---
+++ exercises/List/thactac-list.py
@@ -0,0 +1,23 @@
+# Import
+
+# Function
+
+# Main
+if __name__ == "__main__":
+    print "Main component"
+
+list_used=['Thanh','Thang Long',15,'True']
+print "Chuoi tuong tac: ",list_used
+
+## Them phan tu
+list_used.append('Long Bien HN')
+print "Them phan tu: ",list_used
+
+## Tong phan tu
+print "Tong phan tu: ",list_used.count(list_used)
+
+## Thay doi vi tri
+list_used.reverse()
+print "Thay doi vi tri: ",list_used
+
+##
