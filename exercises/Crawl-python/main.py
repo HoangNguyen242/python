c240a59791b3dc0437ea55bd71b1b0c8c602567b
lacoski <123xuzu@gmail.com>
2018-03-10 01:54:56 +0700

    update

--- exercises/Crawl-python/main.py
+++ exercises/Crawl-python/main.py
@@ -1,29 +1,53 @@
 # import
 import requests 
 import bs4
+import json
+import time
+
 from urllib.parse import urljoin
 
 SOURCE_URL = 'http://vietnamnet.vn/vn/cong-nghe/'
 # function
 
 def get_content_post(CONTENT_URL):
-    pass
+    data = {}
+    r = requests.get(CONTENT_URL)
+    if r.ok:
+        s = bs4.BeautifulSoup(r.content, 'lxml')        
+        title = s.select_one('h1.title').next
+        data['title'] = title 
+        content = s.select_one('#ArticleContent')
+        data['content'] = content
+        time = s.select_one('.ArticleDate').next
+        data['time'] = time 
+        
+    else: 
+        print("khong truy cap duoc")
+    return data 
 
 def get_list_link():
+    list_post = []
     r = requests.get(SOURCE_URL)
     if r.ok:
         s = bs4.BeautifulSoup(r.content, 'lxml')        
         list_obj = s.select('.item.clearfix.dotter > a')
         for obj in list_obj:
+            time.sleep(1)
             #print(obj.attrs['href'])
-            print(urljoin(SOURCE_URL, obj.attrs['href']))
+            #print(urljoin(SOURCE_URL, obj.attrs['href']))
+            link_obj = urljoin(SOURCE_URL, obj.attrs['href'])
+            print(link_obj)
+            list_post.append(get_content_post(link_obj))
+            print("done")
     else: 
         print("khong truy cap duoc")
 
+    return list_post
 # main
 def main():
-    #get_list_title()
-    get_list_link()
+    #print(get_list_link())
+    print(get_content_post('http://vietnamnet.vn/vn/cong-nghe/tin-cong-nghe/samsung-dang-nam-giu-cong-nghe-sac-pin-dot-pha-434821.html'))
+
 
 if __name__ == "__main__":
     main()
