2a4af8f31106a53ec9c14ae79d5d44c2a0567edc
thanh1996 <123xuzu@gmail.com>
2018-03-11 01:04:31 -0800

    update

--- exercises/Tong-hop-bai-tap/crawl-package/main.py
+++ exercises/Tong-hop-bai-tap/crawl-package/main.py
@@ -7,12 +7,12 @@ import os
 def main():    
     crawl_a=crawl_class.crawl_vietnamenet()
     print(crawl_a.TARGET_DIRECTORY)
-    #crawl_a.get_list_post()
-    #crawl_a.crawl_content_of_list_post()
-    crawl_a.LIST_POST.append('http://vietnamnet.vn/vn/cong-nghe/mua-galaxy-s9-s9-tiet-kiem-den-9-trieu-dong-434449.html')
+    crawl_a.get_list_post()
+    crawl_a.crawl_content_of_list_post()
+    #crawl_a.LIST_POST.append('http://vietnamnet.vn/vn/cong-nghe/mua-galaxy-s9-s9-tiet-kiem-den-9-trieu-dong-434449.html')
 
-    print(crawl_a.LIST_POST)
-    crawl_a.get_file_name('http://vietnamnet.vn/vn/cong-nghe/mua-galaxy-s9-s9-tiet-kiem-den-9-trieu-dong-434449.html')
+    #print(crawl_a.LIST_POST)
+    #crawl_a.get_file_name('http://vietnamnet.vn/vn/cong-nghe/mua-galaxy-s9-s9-tiet-kiem-den-9-trieu-dong-434449.html')
     #crawl_a.crawl_content_of_list_post()
 
 if __name__ == "__main__":
